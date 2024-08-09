82c5ff54636047f00682cc5fe626a23494f4c685
OksanaGreen <SpringOcsana@ukr.net>
2024-08-09 22:41:19 +0300

    processing and correction

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+export let perPage = 15;
+import { page } from '../main';
+
+export async function getPictures(query, currentPage) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const params = new URLSearchParams({
+    key: '45354989-e1e4081af9194e4d3149ed2ad ',
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: currentPage,
+    per_page: perPage,
+  });
+  const url = `${BASE_URL}?${params}`;
+
+  const images = await axios
+    .get(url)
+    .then(response => response.data.hits)
+    .catch(error => console.log(error));
+
+  return images;
+}
