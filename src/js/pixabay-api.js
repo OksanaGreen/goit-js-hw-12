64e5cccd40a7cec0a0b01df68e2e29691907bdb0
OksanaGreen <SpringOcsana@ukr.net>
2024-08-12 18:35:56 +0300

    audit key api

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -5,7 +5,7 @@ import { page } from '../main';
 export async function getPictures(query, currentPage) {
   const BASE_URL = 'https://pixabay.com/api/';
   const params = new URLSearchParams({
-    key: '45354989-e1e4081af194e4d3149ed2a9d',
+    key: '42978821-e3c6f538b5791c0a766c3ba19',
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
