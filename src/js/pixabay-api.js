15b1be901292f6819bac1b823f27bc711b915914
OksanaGreen <SpringOcsana@ukr.net>
2024-08-12 18:17:42 +0300

    corect api

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -5,7 +5,7 @@ import { page } from '../main';
 export async function getPictures(query, currentPage) {
   const BASE_URL = 'https://pixabay.com/api/';
   const params = new URLSearchParams({
-    key: '45354989-e1e4081af9194e4d3149ed2ad ',
+    key: '45354989-e1e4081af194e4d3149ed2a9d',
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -8,27 +8,33 @@ export function renderElement({
   downloads,
 }) {
   return `<li class="gallery-item">
-        <a class="gallery-link" href= ${largeImageURL}>  
-            <img class="gallery-image"
-                src= "${webformatURL}"
-                alt = "${tags}"
+          <a class="gallery-link" href="${largeImageURL}">
+            <img
+              class="gallery-image"
+              src="${webformatURL}"
+              alt="${tags}"
+              width="360"
             />
-        </a>
-        <div class="container"><h3 class="header">Likes
-            <p class="views">${likes}</p>
-        </h3>
-        <h3 class="header">Views
-        <p class="views">${views}</p>
-        </h3>
-        <h3 class="header">Comments
-        <p class="views">${comments}</p>
-        </h3>
-        <h3 class="header">Downloads
-        <p class="views">${downloads}</p>
-        </h3>
-        </div>
-        
-    </li>`;
+          </a>
+          <div class="thumb-block">
+            <div class="block">
+              <h2 class="tittle">Likes</h2>
+              <p class="amount">${likes}</p>
+            </div>
+            <div class="block">
+              <h2 class="tittle">Views</h2>
+              <p class="amount">${views}</p>
+            </div>
+            <div class="block">
+              <h2 class="tittle">Comments</h2>
+              <p class="amount">${comments}</p>
+            </div>
+            <div class="block">
+              <h2 class="tittle">Downloads</h2>
+              <p class="amount">${downloads}</p>
+            </div>
+          </div>
+        </li>`;
 }
 
 export function renderElements(arr) {
